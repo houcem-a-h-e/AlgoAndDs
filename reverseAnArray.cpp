7e3457a82d597ed498905987aba78523e56f20a5
houcem-a-he <houcemeddine.aouissaoui@esprit.tn>
2024-09-25 22:43:26 +0200

    reverse an array algorithm

---
+++ reverseAnArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <utility> 
+using namespace std;
+void reverse(int arr[],int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+    }
+}
